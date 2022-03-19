3120a6f5266264a0eb52c124fc70fcb491433740
pmy0792 <pmy0792@naver.com>
2022-03-19 15:05:09 +0200

    add favicon

--- flask-app.py
+++ flask-app.py
@@ -2,14 +2,15 @@ from recom import *
 from flask import Flask
 from flask_restx import Resource,Api
 from flask_cors import CORS
-
+from flask import url_for
 app=Flask(__name__)
 CORS(app)
 
 api=Api(app,version='1.0',title='Recommendation API',description='API for Content Recommendation')
 ns=api.namespace('custom',description='operations')
 app.config.SWAGGER_UI_DOC_EXPANSION='full'
-
+app.add_url_rule('/favicon.ico',
+                 redirect_to=url_for(filename='favicon'))
 #@api.route('/<string:text>')
 @api.response(200,'Found')
 @api.response(404,'Not found')
